2cdf114aa0a76e196055feaaf86167f4c000c9b6
Josh the Man <gta.alpha.activities@gmail.com>
2025-12-15 14:24:58 -0800

    Updated WallpaperRotator.tsx

--- src/components/WallpaperRotator.tsx
+++ src/components/WallpaperRotator.tsx
@@ -17,10 +17,10 @@ export default function WallpaperRotator() {
             background-position: 46% 80px;
           }
           45% {
-            background-position: 85% 80px;
+            background-position: 90% 80px;
           }
           60% {
-            background-position: 85% 80px;
+            background-position: 90% 80px;
           }
           90% {
             background-position: 46% 80px;
